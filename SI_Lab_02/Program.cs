b14d2f9c786502bebec3be053e7bfa397e9250f5
Patryk <patryk-dym@o2.pl>
2020-03-22 19:49:53 +0100

    Init

---
+++ SI_Lab_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SI_Lab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
